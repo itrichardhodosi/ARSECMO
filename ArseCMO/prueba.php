e525335d8cddf2666420007c2e25af1dfa2dfde8
Richard <itrichardhodosi@gmail.com>
2020-04-27 23:49:08 -0600

    PrimercommitArseCMO

---
+++ ArseCMO/prueba.php
@@ -0,0 +1,11 @@
+<?php
+$mailTo = 'richardhodosi23@gmail.com';
+$headers = "From: ".$mailFrom;
+$txt = "You have received an email ";
+
+mail ;
+header("Location: index.php?mailsend");
+
+
+
+}
---
+++ ArseCMO/prueba3-DESKTOP-4G9901S.php
@@ -0,0 +1,6 @@
+<?php
+// the message
+$msg = "First line of text\nSecond line of text";
+// send email
+mail("alex.joffre.a@gmail.com","My subject",$msg);
+?>
